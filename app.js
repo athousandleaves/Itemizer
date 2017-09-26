a3bb0b62b874f387e4b910234270a819c2adef6e
athousandleaves <superamare@gmail.com>
2017-09-26 11:31:52 -0500

    set up initial variables to append to speech object

---
+++ app.js
@@ -0,0 +1,6 @@
+var talking = false;
+var transcript = '';
+var listText = document.getElementById("list");
+var micButton = document.getElementById("micbutton");
+
+listText.style.display = "none";
