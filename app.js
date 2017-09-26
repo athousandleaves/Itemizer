878a4484a092bb223796d29cb71cd7cfe5fdbc0e
athousandleaves <superamare@gmail.com>
2017-09-26 11:32:53 -0500

    set up speech object with browser check

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ var listText = document.getElementById("list");
 var micButton = document.getElementById("micbutton");
 
 listText.style.display = "none";
+
+if (!('webkitSpeechRecognition' in window)) {
+  alert("Web Speech API is not supported by this browser. Please upgrade to Chrome version 25 or later.");
+}
